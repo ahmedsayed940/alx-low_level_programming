482bce71d1d7ec827e2e11c11a43d08f69b6ac5e
ahmedsayed940 <ahmeds.space940@gmail.com>
2023-09-08 09:27:59 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
- * mein - start program
- * refturn: is 0
- */
+ *  * main - description
+ *   * Return: 0 (seccsse)
+ *    */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle\n");
+			return (0);
 }
+
