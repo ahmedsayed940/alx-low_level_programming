db24259b2e615a8cc589fa6c5fe7014f6e0b4d5a
ahmedsayed940 <ahmeds.space940@gmail.com>
2023-09-07 17:55:10 -0700

    compiler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+ * mein - start program
+ * refturn: is 0
+ */
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}
