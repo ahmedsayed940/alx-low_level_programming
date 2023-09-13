4153f7d8753e840e9dc4114fee88c9b8394b126b
ahmedsayed940 <ahmeds.space940@gmail.com>
2023-09-13 07:16:03 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - description
+ * Return: 0 (seccsse)
+ */
+int main(void)
+{
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar('\n');
+	return (0);
+}
