ddc7a9930cc6f747dc39bc3151e4fa35d97987fa
ahmedsayed940 <ahmeds.space940@gmail.com>
2023-09-13 15:12:49 -0700

    time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int d;
+	int c;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 9; b++)
+
+		{
+			for (c = 0; c < = 5; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (a >= 2 && c >= 4)
+					{
+						break;
+					}
+					_putchar(a + 48);
+					_putchar(b + 48);
+					_putchar(58);
+					_putchar(c + 48);
+					_putchar(d + 48);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
