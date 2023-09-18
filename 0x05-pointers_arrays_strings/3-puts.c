dfc581dd477b9da942334e7a9a148d742679422a
ahmedsayed940 <ahmeds.space940@gmail.com>
2023-09-18 16:46:58 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: a string
+ * Return: equal to length of a string
+ */
+void _puts(char *str);
+{
+	int n = 0;
+
+	while (*s != '\0')
+	{
+		_putchar('*str');
+		s++;
+		n++;
+	}
+	_putchar('\n');
+}
