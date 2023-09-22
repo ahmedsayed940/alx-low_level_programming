ac71c61e7a4a03eb35fc42480daeb408e25160ed
ahmedsayed940 <ahmeds.space940@gmail.com>
2023-09-21 17:09:36 -0700

    advanced

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @n: string
+ * Return: n
+ */
+char *rot13(char *n)
+{
+	int i;
+	int j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (n[i] == a[j])
+			{
+				n[i] = b[j];
+			}
+		}
+	}
+	return (n);
+}
