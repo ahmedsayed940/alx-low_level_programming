221c63ae1d7c3254349f69376cc19a0410c7e857
ahmedsayed940 <ahmeds.space940@gmail.com>
2023-09-25 16:24:37 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include <stdio.hd>
+/**
+ * _strspn - function that gets the length of a prefix substrin
+ * @s: strig
+ * @accept: nuber
+ * Return: s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				n++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+			{
+				return (n);
+			}
+		}
+		s++;
+	}
+	return (n);
+}
