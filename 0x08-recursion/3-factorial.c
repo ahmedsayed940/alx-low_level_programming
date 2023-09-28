b4858e43a94abceacee96e274cba097d3e8c48f6
ahmedsayed940 <ahmeds.space940@gmail.com>
2023-09-28 16:23:42 -0700

    hot fix

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * factorial _ function that returns the factorial of a given number
+ * factorial - function that returns the factorial of a given number
  * @n: number
  * Return: returns the factorial of a given number n
  */
