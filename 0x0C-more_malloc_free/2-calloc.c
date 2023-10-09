ec1dba8c550d6fad69aaa6f45da9cbabd53b89cc
ahmedsayed940 <ahmeds.space940@gmail.com>
2023-10-09 16:00:12 -0700

    fix

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *str[];
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	str = malloc(nmemb * size);
+	if (str == NULL)
+		return (NULL);
+	memset(str, 0, nmemb * size);
+	return (str);
+}
