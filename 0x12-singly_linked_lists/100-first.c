40bfa49c72cbb13967f55459c74ee7d7498ccd98
ahmedsayed940 <ahmeds.space940@gmail.com>
2023-10-20 08:30:04 -0700

    100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void print_before_main(void)__attribute__((constructor));
 /**
- * first - prints a sentence before the main
+ * print_before_main - prints a sentence before the main
  * function is executed
  */
 void print_before_main(void)
