efb80c271833876ad30e658a46b341d9ba2da200
ahmedsayed940 <ahmeds.space940@gmail.com>
2024-05-22 14:36:22 -0700

    mandatory

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Linear search algorithm
+ * @array: Array.
+ * @size: Number of elements in array.
+ * @value: Value to search for in the array.
+ *
+ * Return: The first index or -1 if NULL
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
